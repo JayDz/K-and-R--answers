e135a7e2d03f7e8db3da12e9dd8da0078527b9c9
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-09-23 16:22:36 -0400

    Completed exercise 4 of chapter 1.

---
+++ src/chapter_1/1-4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step  = 20;
+
+	celsius = lower;
+
+	printf("%s %s\n","Celsius", "Fahrenheit");
+	while (celsius <= upper) {
+		fahr = celsius * (9.0/5.0) + 32;
+		
+		printf("%3.0f\t%6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
