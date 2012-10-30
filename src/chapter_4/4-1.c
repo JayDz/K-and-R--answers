eca7034eb543dd89d06ceaaf2933a7fd324b8541
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-10-29 23:41:04 -0400

    Completed exercise 1 of chapter 4.

---
+++ src/chapter_4/4-1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int getline2(char line[], int max);
+int strindex(char source[], char searchfor[]);
+int rightmost(char s[]);
+
+char pattern[] = "ould";
+
+main()
+{
+	char line[MAXLINE];
+	int found = 0;
+
+	while (getline2(line, MAXLINE) > 0)
+		if (strindex(line, pattern) >= 0) {
+			printf("%s", line);
+			found++;
+		}
+	return found;
+}
+
+int getline2(char s[], int lim)
+{
+	int c, i;
+
+	i = 0;
+	while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if (c == '\n')
+		s[i++] = c;
+	
+	s[i] = '\0';
+	return i;
+}
+
+int strindex(char s[], char t[])
+{
+	int i,j,k;
+
+	for (i = rightmost(s); s[i] != '\0'; i++) {
+		for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++);
+		if (k > 0 && t[k] == '\0')
+			return i;
+	}
+	return -1;
+}
+
+int rightmost(char s[])
+{
+	int i = strlen(s) / 2;
+	while (s[i] != ' ') i++;
+	return i;
+}
